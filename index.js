edece6a1e9f45fdd5ff1b2594f32d0af62856e12
Mirkocole <mirkocole92@gmail.com>
2024-03-03 19:19:38 +0100

    Final Code

--- index.js
+++ index.js
@@ -25,6 +25,30 @@ const defaultArtist = [
     'bruno mars'
 ];
 
+// Live Comments
+const messages = [
+    "early stage emely syndrome (sett-ott 2022)",
+    "Be The Young",
+    '"..." cit. Kimiko (lug-ago 2022)',
+    "saggio vibes",
+    "main character energy (mag-giu 2022)",
+    "that fucking mood",
+    "VEE, CARLOTTA E GIACOMO VANNO A BOSIO",
+    "An Emily Winchester kind of mood",
+    "landing page (mar-apr 2022)",
+    "2021 lol",
+    "cosa cazzo vuol dire questa affermazione (gen-feb 2022)",
+    "honey and glass (nov-dic 2021)",
+    "(Revenge) Training Arc",
+    "Lidia <3 Emily" ,
+    "minecraft e nintendo switch (sep-oct 2021)",
+    "silvano d'orba? I hardly know her (lug - ago 2021)",
+    "Culo 2021",
+    "Frah Quintale Mix" ,
+    "Francesco Guccini Mix"  
+]
+
+
 let time = 0;
 let audio = new Audio();
 
